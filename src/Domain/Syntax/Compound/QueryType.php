980dd80e81a39dab7181f85babdc61683ae1a631
Hilson <hilsonxhero.me@gmail.com>
2023-03-11 12:44:13 +0330

    Add  must_not query
    ### Added
    - The new must_not query

--- src/Domain/Syntax/Compound/QueryType.php
+++ src/Domain/Syntax/Compound/QueryType.php
@@ -6,11 +6,13 @@ namespace Hilsonxhero\ElasticVision\Domain\Syntax\Compound;
 
 interface QueryType
 {
+    public const MUST_NOT = 'must_not';
+
     public const MUST = 'must';
 
     public const SHOULD = 'should';
 
     public const FILTER = 'filter';
 
-    public const ALL = [self::MUST, self::SHOULD, self::FILTER];
+    public const ALL = [self::MUST, self::MUST_NOT, self::SHOULD, self::FILTER];
 }
